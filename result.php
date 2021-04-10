e756c778faaff682f14185a44a348feae79dbe68
tofuisgood <75235117+judacompany@users.noreply.github.com>
2021-04-10 20:25:20 +0900

    Add files via upload

---
+++ result.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<title>ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 송금낚시성공!</title>
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scaleable=no">
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+
+<link rel="stylesheet" type="text/css" media="all" href="./css/style.css">
+<link rel="stylesheet" type="text/css" media="all" href="./css/news.css">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-23G7HVJHKL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-23G7HVJHKL');
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-23G7HVJHKL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-23G7HVJHKL');
+</script>
+<script data-ad-client="ca-pub-4977790403375949" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
+<body style="padding:10px;">
+	
+	<h1>ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 아뻥!</h1>
+	
+	<div id="counter" style="font-size:1.2rem;font-weight:bold;display:flex;align-items:center;margin-top:2rem;margin-bottom:2rem;justify-content:center">
+	오늘 <img src="https://www.cutercounter.com/hits.php?id=hvxkofxo&nd=6&style=9" border="0" alt="hit counter" style="padding:0 .3rem"> 명이 속아버렸어~
+	</div>
+	
+	<img src="https://mblogthumb-phinf.pstatic.net/20160802_28/jeho89_1470131223185cmKbx_PNG/ĸó6.PNG?type=w800" style="width:100%;">
+	<br><br>
+	
+	<div style="text-align:center">
+	⬇️ 분하다면 클릭<br><br>
+	<a href="http://bank.testpang.com/" class="btn" style="background-color:#F9E000;color:#121212;font-weight:bold;font-size:1.3rem">😂 나도 속이기 [클릭]</a>
+	</div>
+	
+	<a href="https://soonganjeju.modoo.at" target="_blank"><img src="./images/soongan.jpg" style="width:100%;margin-top:2rem"></a>
+	
+</body>
+</html>
